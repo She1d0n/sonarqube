36319119223535591b21cab7ab01a79f97ac06c3
liujiey <liujiey@dcits.com>
2018-12-20 17:56:46 +0800

    *)update api to SQ7.0 *)update version 3.0.0

--- src/test/java/org/sonar/report/pdf/test/PDFPostJobTest.java
+++ src/test/java/org/sonar/report/pdf/test/PDFPostJobTest.java
@@ -17,6 +17,25 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+/*
+  * SonarQube PDF Report
+ * Copyright (C) 2010-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.report.pdf.test;
 
 import static org.mockito.Mockito.mock;
@@ -33,6 +52,7 @@ import org.sonar.report.pdf.batch.PDFPostJob;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 
+@SuppressWarnings("deprecation")
 public class PDFPostJobTest {
 
 	private Project project;
