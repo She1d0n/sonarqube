839bb5e9a57e1c3c7637a176ac673bb0bcb37b91
liujiey <liujiey@dcits.com>
2019-01-03 14:36:24 +0800

    *）增加对开启问题等指标的描述

--- src/main/java/org/sonar/report/pdf/util/MetricKeys.java
+++ src/main/java/org/sonar/report/pdf/util/MetricKeys.java
@@ -25,48 +25,53 @@ package org.sonar.report.pdf.util;
  */
 public enum MetricKeys {
 
-    PROFILE("quality_profiles"), DUPLICATED_LINES("duplicated_lines"), DUPLICATED_BLOCKS(
-            "duplicated_blocks"), DUPLICATED_FILES("duplicated_files"), CLASSES("classes"), COMMENT_LINES(
-                    "comment_lines"), COMPLEXITY("complexity"), FUNCTIONS("functions"), NCLOC("ncloc"),LINES("lines"), DIRECTORIES(
-                            "directories"), FILES("files"),COVERAGE("coverage"), LINE_COVERAGE("line_coverage"), BRANCH_COVERAGE("branch_coverage"),TEST_EXECUTION_TIME(
-                                    "test_execution_time"), SKIPPED_TESTS("skipped_tests"), TESTS("tests"), TEST_ERRORS(
-                                            "test_errors"), TEST_FAILURES("test_failures"), TEST_SUCCESS_DENSITY(
-                                                    "test_success_density"), VIOLATIONS(
-                                                            "violations"), SECURITY(
-																"vulnerabilities"),BUGS(
-																"bugs"),MAINTAINABILITY(
-																"code_smells"),FILE_COMPLEXITY_DISTRIBUTION(
-                                                                    "file_complexity_distribution"), DUPLICATED_LINES_DENSITY(
-                                                                            "duplicated_lines_density"), CLASS_COMPLEXITY(
-                                                                                    "class_complexity"),FILE_COMPLEXITY(
-                                                                                            "file_complexity"), FUNCTION_COMPLEXITY(
-                                                                                            "function_complexity"), COMMENT_LINES_DENSITY(
-                                                                                                    "comment_lines_density"), TECHNICAL_DEBT(
-                                                                                                            "sqale_index"), RELIABILITY_REMEDIATION_EFFORT
-																												("reliability_remediation_effort"),SECUTITY_REMEDIATION_EFFORT
-																													("security_remediation_effort"),BLOCKER_VIOLATIONS(
-                                                                                                                    "blocker_violations"), CRITICAL_VIOLATIONS(
-                                                                                                                            "critical_violations"), MAJOR_VIOLATIONS(
-                                                                                                                                    "major_violations"), MINOR_VIOLATIONS(
-                                                                                                                                            "minor_violations"), INFO_VIOLATIONS(
-                                                                                                                                                    "info_violations");
+	PROFILE("quality_profiles"), DUPLICATED_LINES("duplicated_lines"), DUPLICATED_BLOCKS(
+			"duplicated_blocks"), DUPLICATED_FILES("duplicated_files"), CLASSES("classes"), COMMENT_LINES(
+					"comment_lines"), COMPLEXITY("complexity"), FUNCTIONS("functions"), NCLOC("ncloc"),LINES("lines"), DIRECTORIES(
+							"directories"), FILES("files"),COVERAGE("coverage"), LINE_COVERAGE("line_coverage"), BRANCH_COVERAGE("branch_coverage"),TEST_EXECUTION_TIME(
+									"test_execution_time"), SKIPPED_TESTS("skipped_tests"), TESTS("tests"), TEST_ERRORS(
+											"test_errors"), TEST_FAILURES("test_failures"), TEST_SUCCESS_DENSITY(
+													"test_success_density"), VIOLATIONS(
+															"violations"), SECURITY(
+																	"vulnerabilities"),BUGS(
+																			"bugs"),MAINTAINABILITY(
+																					"code_smells"),FILE_COMPLEXITY_DISTRIBUTION(
+																							"file_complexity_distribution"), DUPLICATED_LINES_DENSITY(
+																									"duplicated_lines_density"), CLASS_COMPLEXITY(
+																											"class_complexity"),FILE_COMPLEXITY(
+																													"file_complexity"), FUNCTION_COMPLEXITY(
+																															"function_complexity"), COMMENT_LINES_DENSITY(
+																																	"comment_lines_density"), TECHNICAL_DEBT(
+																																			"sqale_index"), RELIABILITY_REMEDIATION_EFFORT
+	("reliability_remediation_effort"),SECUTITY_REMEDIATION_EFFORT
+	("security_remediation_effort"),BLOCKER_VIOLATIONS(
+			"blocker_violations"), CRITICAL_VIOLATIONS(
+					"critical_violations"), MAJOR_VIOLATIONS(
+							"major_violations"), MINOR_VIOLATIONS(
+									"minor_violations"),OPEN_ISSUES(
+											"open_issues"),REOPENED_ISSUES(
+													"reopened_issues"),CONFIRMED_ISSUES(
+															"confirmed_issues"),FALSE_POSITIVE_ISSUES(
+																	"false_positive_issues"),WONT_FIX_ISSUES(
+																			"wont_fix_issues"), INFO_VIOLATIONS(
+											"info_violations");
 
-    private final String key;
+	private final String key;
 
-    MetricKeys(String key) {
-        this.key = key;
-    }
+	MetricKeys(String key) {
+		this.key = key;
+	}
 
-    public String getKey() {
-        return key;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public static boolean isMetricNeeded(String key) {
-        for (MetricKeys metric : values()) {
-            if (metric.getKey().equals(key)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public static boolean isMetricNeeded(String key) {
+		for (MetricKeys metric : values()) {
+			if (metric.getKey().equals(key)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
